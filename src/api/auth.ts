7d053e007a73a8e890fe70a143c7c3c6789b59a3
Utinot <levanthuan412002@gmail.com>
2022-09-27 23:43:07 +0700

    signin

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,11 +1,11 @@
 import { instance } from "./instance"
 
 export const signup = (data: any) => {
-   const url = `/register`
+   const url = `/user/register`
    return instance.post(url, data)
 }
 
-export const getData = () => {
-   const url = `/test`
-   return instance.get(url)
+export const signin = (data: any) => {
+   const url = `/user/login`
+   return instance.post(url,data)
 }
--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-   baseURL: 'http://itwork19.herokuapp.com/api/user',
+   baseURL: 'http://datnweb19.herokuapp.com/api',
    headers: {
       'Content-type': 'application/json',
       'Access-Control-Allow-Origin': '*'
