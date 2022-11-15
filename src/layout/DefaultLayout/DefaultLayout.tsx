df201b79d3c6a661b36e5e94343bc3160ce0acd9
khangle23078 <khangle2307@gmail.com>
2022-11-15 17:42:01 +0700

    change js folder to assets folder

--- src/layout/DefaultLayout/DefaultLayout.tsx
+++ src/layout/DefaultLayout/DefaultLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer/Footer";
 
-import "../../js/smoothproducts.js";
+import "../../assets/js/smoothproducts.js";
 import "../../css/styles.css";
 import Header from "./Header/Header";
 import Header2 from "./Header2/Header2";
