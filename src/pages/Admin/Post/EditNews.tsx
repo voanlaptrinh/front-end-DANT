3e49aa0a963e38f92877a9271e2f6f12d34b54ba
hungnp19072002 <hungnv19072002@gmail.com>
2022-11-23 13:54:51 +0700

    up date

--- src/pages/Admin/Post/EditNews.tsx
+++ src/pages/Admin/Post/EditNews.tsx
@@ -48,150 +48,150 @@ const EditNews = (props: Props) => {
   return (
     <div>
       <div>
-      <div className="card-create-employer">
-            <div className="card">
-              <div className="card-header">
-                <h5 className="card-title"> Đăng tin tuyển dụng</h5>
-              </div>
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-6">
+        <div className="card-create-employer">
+          <div className="card">
+            <div className="card-header">
+              <h5 className="card-title"> Đăng tin tuyển dụng</h5>
+            </div>
+            <div className="card-body">
+              <div className="row">
+                <div className="col-6">
 
-                    <div className="mb-4">
-                      <label className="form-label">Tiêu đề<span className="required-lable">*</span></label>
-                      <input type="email" className="form-control" />
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">Số lượng cần tuyển<span className="required-lable">*</span></label>
-                      <input type="number" className="form-control" min="0" />
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">Giới tính<span className="required-lable">*</span></label>
-                      <select className="form-control" required aria-label="select example" >
-                        <option value="">Chọn giới tính</option>
-                        <option value="1">Nam</option>
-                        <option value="2">Nữ</option>
-                      </select>
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">Mô tả công việc<span className="required-lable">*</span></label>
-                      <textarea className="form-control" ></textarea>
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">Yêu cầu công việc<span className="required-lable">*</span></label>
-                      <textarea className="form-control" ></textarea>
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">Quyền lơi công việc<span className="required-lable">*</span></label>
-                      <textarea className="form-control" ></textarea>
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">Chọn nghành nghề<span className="required-lable">*</span></label>
-                      <select className="form-control" required aria-label="select example" >
-                        <option value="">Chọn nghành nghề</option>
-                        <option value="1"></option>
-                        <option value="2"></option>
-                      </select>
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">Chọn nơi làm việc<span className="required-lable">*</span></label>
-                      <select className="form-control" required aria-label="select example" >
-                        <option value="">Chọn nơi làm việc</option>
-                        <option value="1"></option>
-                        <option value="2"></option>
-                      </select>
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">Chọn địa chỉ cụ thể<span className="required-lable">*</span></label>
-                      <input type="text" className="form-control" />
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">Hạn nộp hồ sơ<span className="required-lable">*</span></label>
-                      <input type="date" className="form-control" />
-                    </div>
+                  <div className="mb-4">
+                    <label className="form-label">Tiêu đề<span className="required-lable">*</span></label>
+                    <input type="email" className="form-control" />
                   </div>
-                  <div className="col-6">
-                    <div className="mb-4">
-                      <label className="form-label">Chọn chuyên ngành<span className="required-lable">*</span></label>
-                      <select className="form-control" required aria-label="select example" >
-                        <option value="">Chọn chuyên ngành</option>
-                        <option value="1"></option>
-                        <option value="2"></option>
-                      </select>
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">chọn trình độ<span className="required-lable">*</span></label>
-                      <select className="form-control" required aria-label="select example" >
-                        <option value="">chọn trình độ</option>
-                        <option value="1"></option>
-                        <option value="2"></option>
-                      </select>
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">chọn kinh nghiệm<span className="required-lable">*</span></label>
-                      <select className="form-control" required aria-label="select example" >
-                        <option value="">chọn kinh nghiệm</option>
-                        <option value="1"></option>
-                        <option value="2"></option>
-                      </select>
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">chọn mức lương<span className="required-lable">*</span></label>
-                      <select className="form-control" required aria-label="select example" >
-                        <option value="">chọn mức lương</option>
-                        <option value="1"></option>
-                        <option value="2"></option>
-                      </select>
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">Hình thức làm việc<span className="required-lable">*</span></label>
-                      <select className="form-control" required aria-label="select example" >
-                        <option value="">Hình thức làm việc</option>
-                        <option value="1"></option>
-                        <option value="2"></option>
-                      </select>
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label">Thời gian làm việc<span className="required-lable">*</span></label>
-                      <select className="form-control" required aria-label="select example" >
-                        <option value="">Thời gian làm việc</option>
-                        <option value="1"></option>
-                        <option value="2"></option>
-                      </select>
-                    </div>
-                    <div className="mb-4">
-                      <label className="form-label"> Kỹ năng</label>
+                  <div className="mb-4">
+                    <label className="form-label">Số lượng cần tuyển<span className="required-lable">*</span></label>
+                    <input type="number" className="form-control" min="0" />
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Giới tính<span className="required-lable">*</span></label>
+                    <select className="form-control" required aria-label="select example" >
+                      <option value="">Chọn giới tính</option>
+                      <option value="1">Nam</option>
+                      <option value="2">Nữ</option>
+                    </select>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Mô tả công việc<span className="required-lable">*</span></label>
+                    <textarea className="form-control" ></textarea>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Yêu cầu công việc<span className="required-lable">*</span></label>
+                    <textarea className="form-control" ></textarea>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Quyền lơi công việc<span className="required-lable">*</span></label>
+                    <textarea className="form-control" ></textarea>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Chọn nghành nghề<span className="required-lable">*</span></label>
+                    <select className="form-control" required aria-label="select example" >
+                      <option value="">Chọn nghành nghề</option>
+                      <option value="1"></option>
+                      <option value="2"></option>
+                    </select>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Chọn nơi làm việc<span className="required-lable">*</span></label>
+                    <select className="form-control" required aria-label="select example" >
+                      <option value="">Chọn nơi làm việc</option>
+                      <option value="1"></option>
+                      <option value="2"></option>
+                    </select>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Chọn địa chỉ cụ thể<span className="required-lable">*</span></label>
+                    <input type="text" className="form-control" />
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Hạn nộp hồ sơ<span className="required-lable">*</span></label>
+                    <input type="date" className="form-control" />
+                  </div>
+                </div>
+                <div className="col-6">
+                  <div className="mb-4">
+                    <label className="form-label">Chọn chuyên ngành<span className="required-lable">*</span></label>
+                    <select className="form-control" required aria-label="select example" >
+                      <option value="">Chọn chuyên ngành</option>
+                      <option value="1"></option>
+                      <option value="2"></option>
+                    </select>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">chọn trình độ<span className="required-lable">*</span></label>
+                    <select className="form-control" required aria-label="select example" >
+                      <option value="">chọn trình độ</option>
+                      <option value="1"></option>
+                      <option value="2"></option>
+                    </select>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">chọn kinh nghiệm<span className="required-lable">*</span></label>
+                    <select className="form-control" required aria-label="select example" >
+                      <option value="">chọn kinh nghiệm</option>
+                      <option value="1"></option>
+                      <option value="2"></option>
+                    </select>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">chọn mức lương<span className="required-lable">*</span></label>
+                    <select className="form-control" required aria-label="select example" >
+                      <option value="">chọn mức lương</option>
+                      <option value="1"></option>
+                      <option value="2"></option>
+                    </select>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Hình thức làm việc<span className="required-lable">*</span></label>
+                    <select className="form-control" required aria-label="select example" >
+                      <option value="">Hình thức làm việc</option>
+                      <option value="1"></option>
+                      <option value="2"></option>
+                    </select>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Thời gian làm việc<span className="required-lable">*</span></label>
+                    <select className="form-control" required aria-label="select example" >
+                      <option value="">Thời gian làm việc</option>
+                      <option value="1"></option>
+                      <option value="2"></option>
+                    </select>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label"> Kỹ năng</label>
 
-                      <div className="card-body recuitment-body check-box-employer">
-                        <Form.Item
-                          name="skill_id"
-                          valuePropName="checked"
-                          rules={[
-                            {
-                              required: true,
-                              message: "bạn chưa chọn kĩ năng",
-                            },
-                          ]}
-                        >
-                          {/* <Checkbox.Group
+                    <div className="card-body recuitment-body check-box-employer">
+                      <Form.Item
+                        name="skill_id"
+                        valuePropName="checked"
+                        rules={[
+                          {
+                            required: true,
+                            message: "bạn chưa chọn kĩ năng",
+                          },
+                        ]}
+                      >
+                        {/* <Checkbox.Group
                             options={categories.skill?.map((skill: any) => ({
                               label: skill.name,
                               value: skill.id,
                             }))}
                           ></Checkbox.Group> */}
-                        </Form.Item>
-                      </div>
-
-
+                      </Form.Item>
                     </div>
+
+
                   </div>
                 </div>
-                <div className="rec-submit">
-                  <button type="button" className="btn btn-primary">Lưu Tin</button>
-                </div>
+              </div>
+              <div className="rec-submit">
+                <button type="button" className="btn btn-primary">Lưu Tin</button>
               </div>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
